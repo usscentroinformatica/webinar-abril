f1c45d7a2b8437bb16df66a937a8866a3bc4b49b
usscentroinformatica <usscentroinformatica@gmail.com>
2026-03-26 11:44:13 -0500

    Remove unused SVG icon components
    Removed unused SVG icon components (BookIcon, UserIcon, EmailIcon) from EncuestaWebinar.

--- src/components/EncuestaWebinar.jsx
+++ src/components/EncuestaWebinar.jsx
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logoUss from '../assets/uss.png';
 
-// Iconos SVG
-const BookIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#5a2290" strokeWidth="2.5">
-    <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
-    <path d="M6 2h14a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z" />
-  </svg>
-);
-
-const UserIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#63ed12" strokeWidth="2.5">
-    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-    <circle cx="12" cy="7" r="4" />
-  </svg>
-);
-
-const EmailIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#5a2290" strokeWidth="2.5">
-    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-    <polyline points="22,6 12,13 2,6" />
-  </svg>
-);
-
 const SuccessIcon = () => (
   <svg width="100" height="100" viewBox="0 0 100 100" fill="none">
     <circle cx="50" cy="50" r="45" fill="#63ed12" />
